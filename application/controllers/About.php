2b9a3120deae7f3511386c745a4c87f553625ae5
Vijay <vijayvnairv6@gmail.com>
2018-04-07 11:35:42 +0530

    Initial push

--- application/controllers/About.php
+++ application/controllers/About.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
@@ -26,32 +25,3 @@ class About extends CI_Controller {
 		$this->load->view('about');
 	}
 }
-=======
-<?php
-defined('BASEPATH') OR exit('No direct script access allowed');
-
-class About extends CI_Controller {
-
-	/**
-	 * Index Page for this controller.
-	 *
-	 * Maps to the following URL
-	 * 		http://example.com/index.php/welcome
-	 *	- or -
-	 * 		http://example.com/index.php/welcome/index
-	 *	- or -
-	 * Since this controller is set as the default controller in
-	 * config/routes.php, it's displayed at http://example.com/
-	 *
-	 * So any other public methods not prefixed with an underscore will
-	 * map to /index.php/welcome/<method_name>
-	 * @see https://codeigniter.com/user_guide/general/urls.html
-	 */
-	public function index()
-	{
-        $data["title"] = "About";
-        $this->load->view('header',$data);
-		$this->load->view('about');
-	}
-}
->>>>>>> 83bc62bebd700317d3775f4348765b3f2dff914e
--- application/views/header.php
+++ application/views/header.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <!DOCTYPE html>
 <!--  This site was created in Webflow. http://www.webflow.com  -->
 <!--  Last Published: Tue Dec 19 2017 09:13:50 GMT+0000 (UTC)  -->
@@ -45,52 +44,4 @@
 
   gtag('config', 'UA-111863189-1');
 </script>
-=======
-<!DOCTYPE html>
-<!--  This site was created in Webflow. http://www.webflow.com  -->
-<!--  Last Published: Tue Dec 19 2017 09:13:50 GMT+0000 (UTC)  -->
-<html data-wf-page="594137a0bb1abd503180cb48" data-wf-site="594137a0bb1abd503180cb3d">
-
-<!-- Mirrored from www.marcomarino.design/ by HTTrack Website Copier/3.x [XR&CO'2014], Mon, 05 Mar 2018 11:12:45 GMT -->
-<!-- Added by HTTrack --><meta http-equiv="content-type" content="text/html;charset=utf-8" /><!-- /Added by HTTrack -->
-<head>
-  <meta charset="utf-8">
-  <title><?php echo $title;?></title>
-  <meta content="Marco Marino is a visual &amp; UI/UX designer born in Italy, currently living and working in Barcelona." name="description">
-  <meta content="Marco Marino | UI/UX Designer" property="og:title">
-  <meta content="Marco Marino is a visual &amp; UI · UX designer born in Italy, currently living and working in Barcelona." property="og:description">
-  <meta content="http://www.marcomarino.design/2016/opengraph/marcomarino.jpg" property="og:image">
-  <meta content="summary" name="twitter:card">
-  <meta content="width=device-width, initial-scale=1" name="viewport">
-  <meta content="Webflow" name="generator">
-  <link href="<?php echo base_url();?>assets/css/normalize.css" rel="stylesheet" type="text/css">
-  <link href="<?php echo base_url();?>assets/css/webflow.css" rel="stylesheet" type="text/css">
-  <link href="<?php echo base_url();?>assets/css/iperspace.webflow.css" rel="stylesheet" type="text/css">
-  <link rel="stylesheet" type="text/css" href="<?php echo base_url();?>assets/css/demo.css" />
-  <link rel="stylesheet" type="text/css" href="<?php echo base_url();?>assets/css/awwwards.css" />
-    <script>document.documentElement.className="js";var supportsCssVars=function(){var e,t=document.createElement("style");return t.innerHTML="root: { --tmp-var: bold; }",document.head.appendChild(t),e=!!(window.CSS&&window.CSS.supports&&window.CSS.supports("font-weight","var(--tmp-var)")),t.parentNode.removeChild(t),e};supportsCssVars()||alert("Please view this demo in a modern browser that supports CSS Variables.");</script>
-  <script src="http://ajax.googleapis.com/ajax/libs/webfont/1.4.7/webfont.js" type="text/javascript"></script>
-  <script type="text/javascript">WebFont.load({  google: {    families: ["Asul:regular,700","Rubik:300,regular,500,700"]  }});</script>
-  <!-- [if lt IE 9]><script src="https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv.min.js" type="text/javascript"></script><![endif] -->
-  <script type="text/javascript">!function(o,c){var n=c.documentElement,t=" w-mod-";n.className+=t+"js",("ontouchstart"in o||o.DocumentTouch&&c instanceof DocumentTouch)&&(n.className+=t+"touch")}(window,document);</script>
-  <link href="<?php echo base_url();?>assets/images/fivicon.jpg" rel="shortcut icon" type="image/x-icon">
-  <link href="<?php echo base_url();?>assets/images/1.jpg" rel="apple-touch-icon">
-  <style>
-.bg-video, .w-nav-overlay, .w-tab-content, body {
-    overflow-x: hidden;
-}
-.grid-block {
-    overflow-x: hidden;
-}
-</style>
-<!-- Global site tag (gtag.js) - Google Analytics -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=UA-111863189-1"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'UA-111863189-1');
-</script>
->>>>>>> 83bc62bebd700317d3775f4348765b3f2dff914e
 </head>
